305572b2c470cf85ed76362d42c04362ac267638
Errett <errett.bentz@gmail.com>
2025-04-20 15:47:17 -0500

    finished char count and correct printing

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def get_book_text(file_path):
   return file_contents
 
 def main():
-  num_words = count_words(get_book_text("./books/frankenstein.txt"))
+  book_text = get_book_text("./books/frankenstein.txt")
+  num_words = count_words(book_text)
   print(f"{num_words} words found in the document")
+  num_chars = count_chars(book_text)
+  for char in num_chars:
+    print(f"\'{char}\': {num_chars[char]}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def count_words(book_text = "book_text"):
   book_words = book_text.split()
   return len(book_words)
+
+def count_chars(book_text = "book_text"):
+  char_counts = dict()
+  for letter in book_text:
+    char = letter.lower()
+    if char in char_counts:
+      char_counts[char] += 1
+    elif char == '\n':
+      pass
+    else:
+      char_counts[char] = 1
+  return char_counts
