865539ebfa7a1e2293e97a5130c07ce25569335a
Errett <errett.bentz@gmail.com>
2025-04-21 00:20:30 -0500

    added error handling for bad filepath

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from stats import *
 import sys as s
 
 def get_book_text(file_path):
-  file_contents = ""
-  with open(file_path) as book:
-    file_contents = book.read()
-  return file_contents
+  try:
+    file_contents = ""
+    with open(file_path) as book:
+      file_contents = book.read()
+    return file_contents
+  except FileNotFoundError:
+    print(f"{file_path} does not exist")
+    s.exit(1)
 
 def print_sorted_counts(file_path = "", num_words = 0, num_chars_dict_list = [{}]):
   print("============ BOOKBOT ============")
@@ -31,4 +35,6 @@ def main():
                       sort_char_counts(count_chars(get_book_text(file_path)))
                      )
 
+
 main()
+
